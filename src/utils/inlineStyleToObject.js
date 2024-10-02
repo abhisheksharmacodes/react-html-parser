a9013e5f1860cd4553da3858dd7756fe9125be64
Abhishek Sharma <geekysharma31@gmail.com>
2024-10-02 17:08:27 +0530

    Undo changes

--- src/utils/inlineStyleToObject.js
+++ src/utils/inlineStyleToObject.js
@@ -34,7 +34,7 @@ export default function InlineStyleToObject(inlineStyle = '') {
         //      -webkit-style-property = WebkitStyleProperty
         property = property
           .replace(/^-ms-/, 'ms-')
-          .replace(/-(.)/g, (_, character) => character);
+          .replace(/-(.)/g, (_, character) => character.toUpperCase());
 
         // add the new style property and value to the style object
         styleObject[property] = value;
